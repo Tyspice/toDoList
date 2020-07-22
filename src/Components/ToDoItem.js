a30b9e0f708603121f5df8702dce94ac61cf1e01
Tyspice <tyner27@gmail.com>
2020-07-21 23:02:05 -0700

    added more icons and gave em steez

--- src/Components/ToDoItem.js
+++ src/Components/ToDoItem.js
@@ -2,30 +2,37 @@ import React from 'react';
 import '../CSS/ToDoItem.css';
 import { ReactComponent as CloseIcon } from '../Icons/close-outline.svg';
 import { ReactComponent as CheckmarkIcon } from '../Icons/checkmark-outline.svg';
+import { ReactComponent as UpIcon } from '../Icons/chevron-up-outline.svg';
+import { ReactComponent as DownIcon } from '../Icons/chevron-down-outline.svg';
+
 
 class ToDoItem extends React.Component {
     constructor(props) {
         super(props);
         
-        this.handleClick = this.handleClick.bind(this);
+        this.handleClose = this.handleClose.bind(this);
     }
 
-    handleClick() {
+    handleClose() {
         this.props.removeToDoFn(this.props.itemIndex);
+    }
+
+    handleCheck() {
+        // will handle the check btn
+    }
+
+    handleUp() {
+        // will handle the up btn
+    }
+
+    handleDown() {
+        // will handle the dwn btn
     }
 
     render() {
 
-        
-        let iconStyles = {
-            height: 25,
-            width: 25
-        };
-
         return (
-            <div
-            className={ 'todo-container' } 
-            >
+            <div className={ 'todo-container' }>
                 <div className={ 'input' }>
                     {this.props.item}
                 </div>
@@ -33,7 +40,13 @@ class ToDoItem extends React.Component {
                     <div className={ 'icon-container' }>
                         <CheckmarkIcon className={ 'icon' } />
                     </div>
-                    <div className={ 'icon-container' } onClick={ this.handleClick }>
+                    <div className={ 'icon-container' }>
+                        <UpIcon className={ 'icon' } />
+                    </div>
+                    <div className={ 'icon-container' }>
+                        <DownIcon className={ 'icon' } />
+                    </div>
+                    <div className={ 'icon-container' } onClick={ this.handleClose }>
                         <CloseIcon className={ 'icon' } />
                     </div>
                 </div>
